5d102be0213535272a2a3e6e43273ac795327ff8
Chau Anh Le <chauanhle@Chaus-MacBook-Pro.local>
2024-02-14 02:39:57 -0500

    Update with inheritance & composition

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,5 +1,19 @@
 package edu.cmu.cs.cs214.rec04;
 
+/*
+ * We have provided an empty DelegationSortedIntList class where you should 
+ * implement your composition-based solution.
+
+Hints:
+
+Make sure you check that getTotalAdded works with both add and addAll.
+After you have implemented the DelegationSortedIntList class, test the 
+instrumentation you just added using tests in DelegationSortedIntListTest class.
+Run the tests and make sure your instrumentation passes all the tests. (You might want 
+to use the printList helper we provided you and read the documentation for addAll in 
+the AbstractIntList class.)
+ */
+
 /**
  * DelegationSortedIntList -- a variant of a SortedIntList that keeps
  * count of the number of attempted element insertions (not to be confused
@@ -10,7 +24,47 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+    private SortedIntList list = new SortedIntList();
+    private int count;
 
+    public DelegationSortedIntList() {
+        this.list = list;
+        this.count = 0;
+    }
+
+    public boolean add(int num) {
+        count++;
+        return list.add(num);
+    }
+
+    public boolean addAll(IntegerList intList) {
+        int size = intList.size(); 
+        for (int i = 0; i < size; i++) {
+            count++;
+            list.add(list.get(i));
+        }
+        return true;
+    }
+
+    public int get(int index) {
+        return list.get(index);
+    }
+
+    public boolean remove(int num) {
+        return list.remove(num);
+    }
+
+    public boolean removeAll(IntegerList list) {
+        return list.removeAll(list);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public int getTotalAdded() {
+        return this.count;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,46 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
+    private int count;
+
     // Write your implementation below with API documentation
+
+    /**
+     * Adds the specified int to the list. Increase the counter.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override 
+    public boolean add (int num) {
+        count++;
+        return super.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     * Increase the counter.
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override 
+    public boolean addAll(IntegerList list) {
+        int size = list.size(); 
+        for (int i = 0; i < size; i++) {
+            count++;
+            super.add(super.get(i));
+        }
+        return true;
+    }
+
+    /**
+     * Gets the counter.
+     * @return the number of attempted element insertions
+     */
+    public int getTotalAdded() {
+        return this.count;
+    }
+
 
 }
